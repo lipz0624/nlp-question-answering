b96ccea75686d45b6d988ec5797829d8d663e3bb
NovAmberrrr <56175304+NovAmberrrr@users.noreply.github.com>
2020-10-28 05:18:10 -0400

    sample answer extraction

--- answer_extraction.py
+++ answer_extraction.py
@@ -1,13 +1,6 @@
 from question_process import *
 from passage_retrieval import *
 
-def add_score(answer,rank):
-    if answer not in rank:
-        rank[answer] = {'score':1}
-    else:
-        rank[answer]['score'] += 1
-    return rank
-
 def rank_answer(passages,question):
     """
     Input: a list of relevant passages(string) and question
@@ -16,8 +9,6 @@ def rank_answer(passages,question):
     2. Pattern match
     3.# of question keywords in the candidate
     """
-    ##dict to store possible answers and its score
-    rank = {}
     ##expected answer type
     answerType = answerTypeDetection(nlp,question)
     keyQuery = queryFormulation(nlp,question)
@@ -28,19 +19,17 @@ def rank_answer(passages,question):
                 if entity.label_ == "PERSON" or entity.label_ == "NORP" or entity.label_ == "ORG":
                     #We want to include all these NE as possible answers
                     answer = entity.text
-                    ##TODO: answer should not be only one word
-                    rank = add_score(answer,rank)
-                    ##TODO :regular expression pattern matches the candidate
-                    for word in doc: ##features: number of question keywords in the candidate
-                        if word.lemma_ in keyQuery:
-                            rank = add_score(answer,rank)
+                    q_str = ' '.join(keyQuery)
+                    if answer in q_str:##we dont want entity occured in the question
+                        continue
+                    # ##TODO: answer should not be only one word
+                    # and if there are multiple candidate answers, need ranking
         elif answerType == "LOCATION":
             for entity in doc.ents:
                 if entity.label_ == "GPE" or entity.label_ == "LOC" or entity.label_ == "FAC":
                     #We want to include all these NE as possible answers
                     answer = entity.text
-                    rank = add_score(answer,rank)
-                    for word in doc: ##features: number of question keywords in the candidate
-                        if word.lemma_ in keyQuery:
-                            rank = add_score(answer,rank)
-    return -1
+                    q_str = ' '.join(keyQuery)
+                    if answer in q_str:##we dont want entity occured in the question
+                        continue
+    return answer
